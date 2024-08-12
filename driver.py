3d51fa9a3c8c6145058c2dd57492aeddfab7d91e
j-gemma <jacobg391@gmail.com>
2024-08-12 15:41:59 -0400

    blah

---
+++ driver.py
@@ -0,0 +1,12 @@
+# want to know the module and signal type of the last module in the graph
+
+from deap import base, creator, tools
+from pyke import krb_traceback, knowledge_engine
+import random
+
+my_engine = knowledge_engine.engine("C:/Users/jgemma/Programming/Python/caps")
+
+
+synth = [[0], [0], [0], [5, 1, 4, 6, 295], [9, 2, 10, 5, 14773], [0, 2, 9, 7, 14300], [3, 5, 6, 1, 9520], [7, 9, 3, 3, 4108], [4, 3, 2, 4, 15775], [1]]
+
+
